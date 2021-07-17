1585c80afcf8e1bce06317617770b6720bd3e1bc
kenfack2708 <arit.sandra@yahoo.fr>
2021-07-17 23:27:43 +0200

    5th commit

--- src/main/java/net/codejava/javaee/HelloServlet.java
+++ src/main/java/net/codejava/javaee/HelloServlet.java
@@ -53,7 +53,7 @@ public class HelloServlet extends HttpServlet {
 		
 		PrintWriter writer = response.getWriter();
 		writer.println("<h1>Hi " + yourName + " dein Bonus lautet " + bonus +" Euro</h1>");
-		writer.println("Ende Stéphane111");
+		writer.println("Ende Stéphane112");
 		
 		writer.close();
 		// TODO Auto-generated method stub
